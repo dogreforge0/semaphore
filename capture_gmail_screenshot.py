db5e10662a400cb11ea8c885de7a0133aa94fee4
dogreforge0 <dog0@futureprojects.cloudns.org>
2024-12-31 17:11:05 +0100

    Update capture_gmail_screenshot.py

--- capture_gmail_screenshot.py
+++ capture_gmail_screenshot.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # Setup Chrome options for headless mode
@@ -16,13 +19,23 @@ driver = webdriver.Chrome(options=chrome_options)
 try:
     # Open Gmail login page
     driver.get("https://mail.google.com/")
-
-    # Wait for Gmail to load
+    
+    # Wait for the "Create account" button to be clickable
+    create_account_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[text()='Create account']"))
+    )
+    
+    # Click on the "Create account" button
+    create_account_button.click()
+    print("Clicked on the 'Create account' button")
+    
+    # Wait for a few seconds to let the page load after clicking
     time.sleep(5)
-
-    # Take a screenshot of the page
+    
+    # Take a screenshot of the page after clicking the button
     driver.save_screenshot("gmail_screenshot.png")
     print("Screenshot taken and saved as 'gmail_screenshot.png'")
 
 finally:
+    # Close the WebDriver
     driver.quit()
